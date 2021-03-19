b0ff1d47adb8da62179a69e4b8e3d0f1478faa59
ReallySouna <zirlax@outlook.com>
2021-03-19 11:18:48 +0100

    edit permissions (not tested)

--- commands/Staff/ban.js
+++ commands/Staff/ban.js
@@ -1,21 +1,23 @@
 exports.run = async (client, message, args, getPlayer, getUser) => {
-  if (!client.config.owners.includes(message.author.id)) return message.react("❌");
+    if (!client.config.owners.includes(message.author.id) || !message.member.hasPermission("ADMINISTRATOR")) return message.react("❌");
     var con = client.connection
 
     const someone = message.mentions.members.first();
 
     if (!someone) {
-        return message.reply('please mention an user.');
+        return message.reply("please mention an user.");
     }
-    con.query(`UPDATE data SET ban = 1 WHERE userid = ${someone.id}`);
-    message.channel.send(`${someone} a été banni !`)
+    if (!client.config.owners.includes(someone.id)) {
+        con.query(`UPDATE data SET ban = 1 WHERE userid = ${someone.id}`);
+        message.channel.send(`${someone} a été banni !`);
+    }
 };
 
 exports.help = {
-  name: "ban",
-  description_fr: "Pour bannir un joueur du bot",
-  description_en: "To ban a player from the bot",
-  usage_fr: "<joueur>",
-  usage_en: "<player>",
-  category: "Staff"
+    name: "ban",
+    description_fr: "Pour bannir un joueur du bot",
+    description_en: "To ban a player from the bot",
+    usage_fr: "<joueur>",
+    usage_en: "<player>",
+    category: "Staff"
 };
