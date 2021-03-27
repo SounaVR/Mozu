76ba5b13028de23d67e04b6cd8922a85f018c526
ReallySouna <zirlax@outlook.com>
2021-03-27 02:35:00 +0100

    updated query

--- commands/RPG/enchant.js
+++ commands/RPG/enchant.js
@@ -8,19 +8,19 @@ async function manageEnchant(client, con, player, message, category, object, obj
     const lang = require(`../../utils/text/${player.data.lang}.json`);
     const react = ['780222056007991347', '780222833808506920'];
 
-    const level = Math.floor(player.data[objectName])+1;
+    const level = Math.floor(player.enchant[objectName])+1;
 
     const embed = new Discord.MessageEmbed()
     .setColor(message.member.displayColor);
 
     //const objectRessource = Enchant[category][object][1];
-    const getNeededRessource = (player.data[objectName] * player.data[objectName] * 5)+1;
+    const getNeededRessource = (player.enchant[objectName] * player.enchant[objectName] * 5)+1;
 
     embed.setTitle(`Enchant your item ?`)
     let txt = [];
 
-    if (player.data[`rune_${object}`] < getNeededRessource) txt.push(`${Emotes.enchant[`rune_${object}`]} rune_${object} : ${nFormatter(getNeededRessource)} (${Emotes.cancel} - Missing ${nFormatter(Math.floor(getNeededRessource-player.data[`rune_${object}`]))})`);
-    if (player.data[`rune_${object}`] >= getNeededRessource) txt.push(`${Emotes.enchant[`rune_${object}`]} rune_${object} : ${nFormatter(getNeededRessource)} (${Emotes.checked})`);
+    if (player.ress[`rune_${object}`] < getNeededRessource) txt.push(`${Emotes.enchant[`rune_${object}`]} rune_${object} : ${nFormatter(getNeededRessource)} (${Emotes.cancel} - Missing ${nFormatter(Math.floor(getNeededRessource-player.ress[`rune_${object}`]))})`);
+    if (player.ress[`rune_${object}`] >= getNeededRessource) txt.push(`${Emotes.enchant[`rune_${object}`]} rune_${object} : ${nFormatter(getNeededRessource)} (${Emotes.checked})`);
 
     embed.addField("**Cost**", txt);
 
@@ -41,12 +41,14 @@ async function manageEnchant(client, con, player, message, category, object, obj
                 let need = [];
                 let resssql = [];
 
-                if (player.data[`rune_${object}`] < getNeededRessource) need.push(`sorry bro`);
+                if (player.ress[`rune_${object}`] < getNeededRessource) need.push(`sorry bro`);
                 resssql.push(`rune_${object} = rune_${object} - ${getNeededRessource}`);
 
                 if (need.length >= 1) return message.channel.send(`${lang.enchant.notEnoughRess}`);
 
-                con.query(`UPDATE data SET ${resssql.join(',')}, ATK = ${player.data.ATK + Number(Enchant[category][object][0].ATK)}, DEF = ${player.data.DEF + Number(Enchant[category][object][0].DEF)}, ${objectName} = ${level} WHERE userid = ${message.author.id}`);
+                con.query(`UPDATE ress SET ${resssql.join(',')} WHERE userid = ${message.author.id}`);
+                con.query(`UPDATE data SET ATK = ${player.data.ATK + Number(Enchant[category][object][0].ATK)}, DEF = ${player.data.DEF + Number(Enchant[category][object][0].DEF)} WHERE userid = ${message.author.id}`);
+                con.query(`UPDATE enchant SET ${objectName} = ${level} WHERE userid = ${message.author.id}`);
 
                 return message.channel.send(`${lang.enchant.enchantSuccess} : **${level}** !`);
 
@@ -62,7 +64,7 @@ async function manageEnchant(client, con, player, message, category, object, obj
 exports.run = async (client, message, args, getPlayer, getUser) => {
     const con = client.connection
     const player = await getPlayer(con, message.author.id);
-    if (!player) return message.channel.send(`${Default.notRegistered}`);
+    if (!player) return message.channel.send(Default.notRegistered);
     const lang = require(`../../utils/text/${player.data.lang}.json`);
 
     const enchantEmbed = new Discord.MessageEmbed()
