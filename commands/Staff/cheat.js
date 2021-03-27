9e6b822c0680f337f43be94218e1b4fc3d7851cb
ReallySouna <zirlax@outlook.com>
2021-03-27 02:40:27 +0100

    new args : <table>

--- commands/Staff/cheat.js
+++ commands/Staff/cheat.js
@@ -1,8 +1,6 @@
 exports.run = async (client, message, args, getPlayer, getUser) => {
   if (!client.config.owners.includes(message.author.id)) return message.react("❌");
     var con = client.connection
-    var player = await getPlayer(con, message.author.id);
-    if (!player) return message.channel.send("You are not registered, please do the `m!profile` command to remedy this.");
 
     const someone = message.mentions.members.first();
 
@@ -11,16 +9,24 @@ exports.run = async (client, message, args, getPlayer, getUser) => {
     if (!someone) return message.react("❌");
 
     if (args[1] === "set") {
-        if (!args[2]) return message.react("❌");
         if (!args[3]) return message.react("❌");
+        if (!args[4]) return message.react("❌");
 
-        con.query(`UPDATE data SET ${args[2]} = ${args[3]} WHERE userid = ${someone.id}`);
+        switch (args[2]) {
+            case "data": case "ress": case "items": case "enchant": case "prospect": case "stats":
+                con.query(`UPDATE ${args[2]} SET ${args[3]} = ${args[4]} WHERE userid = ${someone.id}`);
+                break;
+        }
         return message.react("✅");
     } else if (args[1] === "add") {
         if (!args[2]) return message.react("❌");
         if (!args[3]) return message.react("❌");
 
-        con.query(`UPDATE data SET ${args[2]} = ${member.data[args[2]] + Number(args[3])} WHERE userid = ${someone.id}`);
+        switch (args[2]) {
+            case "data": case "ress": case "items": case "enchant": case "prospect": case "stats":
+                con.query(`UPDATE ${args[2]} SET ${args[3]} = ${member[args[2]][args[3]] + Number(args[4])} WHERE userid = ${someone.id}`);
+                break;
+        }
         return message.react("✅");
     } else return message.react("❌");
 };//✅ ❌
@@ -29,8 +35,8 @@ exports.help = {
     name: "cheat",
     description_fr: "🕵️",
     description_en: "🕵️",
-    usage_fr: "<@utilisateur> <set/add> <donnée> <nombre>",
-    usage_en: "<@user> <set/add> <data> <number>",
+    usage_fr: "<@utilisateur> <set/add> <table> <donnée> <nombre>",
+    usage_en: "<@user> <set/add> <table> <data> <number>",
     category: "Staff",
     aliases: ["super", "ez"]
 };
