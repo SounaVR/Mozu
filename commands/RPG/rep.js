b305d256017129e1e2aeec3269e2b96761dbd3e2
ReallySouna <zirlax@outlook.com>
2021-03-27 02:38:38 +0100

    indentation | little better

--- commands/RPG/rep.js
+++ commands/RPG/rep.js
@@ -1,12 +1,15 @@
+const Default = require('../../utils/default.json');
+
 module.exports.run = async (client, message, args, getPlayer, getUser) => {
     var con = client.connection
     const player = await getPlayer(con, message.author.id);
-    if (!player) return message.channel.send("You are not registered, please do the `m!village` command to remedy this.");
+    if (!player) return message.channel.send(Default.notRegistered);
     const lang = require(`../../utils/text/${player.data.lang}.json`);
     const user = message.mentions.users.first() || message.author;
     const userid = message.author.id;
 
-    if (!args[0]) return message.channel.send(`${lang.rep.correctUsage}`)
+    if (!args[0] && player.data.lastRep === 1) return message.reply(`${lang.rep.notNow}`);
+    if (!args[0] && player.data.lastRep === 0) return message.reply(`${lang.rep.now}`)
 
     if (user.id === userid) return message.reply(`${lang.rep.giveToSelf}`)
     if (user.id === client.user.id) return message.reply(`${lang.rep.giveToMozu}`)
@@ -14,11 +17,10 @@ module.exports.run = async (client, message, args, getPlayer, getUser) => {
     const member = await getUser(con, user.id);
 
     if (!member) return message.channel.send(`${lang.rep.unknownUser}`);
-    if (player.data.LastRep === 1) {
+    if (player.data.lastRep === 1) {
         return message.reply(`${lang.rep.notNow}`)
-    } else if (player.data.LastRep === 0) {
-        con.query(`UPDATE data SET LastRep = 1 WHERE userid = ${userid}`)
-        con.query(`UPDATE data SET rep = ${member.data.LastRep + Number(1)} WHERE userid = ${user.id}`)
+    } else if (player.data.lastRep === 0) {
+        con.query(`UPDATE data SET lastRep = 1, rep = ${member.data.lastRep + Number(1)} WHERE userid = ${userid}`)
         return message.channel.send(`<@${userid}> - ${lang.rep.done} <@${user.id}>.`)
     }
 }
