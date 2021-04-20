c434bedd72341cd99fbf44698d6d8d987728d6ce
ReallySouna <zirlax@outlook.com>
2021-04-20 02:28:50 +0200

    gems added

--- sql/prospect.sql
+++ sql/prospect.sql
@@ -1,4 +1,11 @@
 CREATE TABLE IF NOT EXISTS prospect (
     uuid bigint,
-    userid bigint
+    userid bigint,
+
+    sapphire bigint,
+    amber bigint,
+    citrine bigint,
+    ruby bigint,
+    jade bigint,
+    amethyst bigint
 )
