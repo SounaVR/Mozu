c3819e4f2087ed507f7b31abacab576488909fb7
ReallySouna <zirlax@outlook.com>
2021-04-20 02:29:33 +0200

    role alpha tester

---
+++ utils/reaction_role/role-claim.js
@@ -0,0 +1,61 @@
+const firstMessage = require('./first-message')
+
+module.exports = (client) => {
+  const channelId = '831131581443342336'
+
+  const getEmoji = (emojiName) =>
+    client.emojis.cache.find((emoji) => emoji.name === emojiName)
+
+  const emojis = {
+    "Alpha_Tester": 'Alpha Tester'
+  }
+
+  const reactions = []
+
+  let emojiText = 'Ajoutez la réaction que vous voulez pour récupérer le rôle correspondant.\n\n'
+  for (const key in emojis) {
+    const emoji = getEmoji(key)
+    reactions.push(emoji)
+
+    const role = emojis[key]
+    emojiText += `${emoji} = ${role}\n`
+  }
+
+  firstMessage(client, channelId, emojiText, reactions)
+
+  const handleReaction = (reaction, user, add) => {
+    if (user.id === '689876535850696715') {
+      return
+    }
+
+    const emoji = reaction._emoji.name
+
+    const { guild } = reaction.message
+
+    const roleName = emojis[emoji]
+    if (!roleName) {
+      return
+    }
+
+    const role = guild.roles.cache.find((role) => role.name === roleName)
+    const member = guild.members.cache.find((member) => member.id === user.id)
+
+    if (add) {
+      member.roles.add(role)
+    } else {
+      member.roles.remove(role)
+    }
+  }
+
+  client.on('messageReactionAdd', (reaction, user) => {
+    if (reaction.message.channel.id === channelId) {
+      handleReaction(reaction, user, true)
+    }
+  })
+
+  client.on('messageReactionRemove', (reaction, user) => {
+    if (reaction.message.channel.id === channelId) {
+      handleReaction(reaction, user, false)
+    }
+  })
+}
