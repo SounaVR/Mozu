e979ef7c6dcec2e78f1f5a0405edbd6be854eb81
ReallySouna <zirlax@outlook.com>
2021-04-22 03:28:08 +0200

    function moved

--- commands/RPG/prospect.js
+++ commands/RPG/prospect.js
@@ -1,64 +1,8 @@
-const { nFormatter } = require('../../utils/u');
-const Discord = require('discord.js'),
-    Emotes    = require('../../utils/emotes.json'),
-    Default   = require('../../utils/default.json');
-
-async function manageProspecting(client, con, player, message, ore, quantity, gem) {
-    const lang = require(`../../utils/text/${player.data.lang}.json`);
-    const react = ['780222056007991347', '780222833808506920'];
-
-    const embed = new Discord.MessageEmbed()
-    .setColor(message.member.displayColor);
-
-    const getNeededRessource = quantity * 10000;
-
-    embed.setTitle(`Do you want to prospect that ?`)
-    let txt = [];
-
-    if (player.ress[ore] < getNeededRessource) txt.push(`${Emotes[ore]} ${ore} : ${nFormatter(getNeededRessource)} (${Emotes.cancel} - Missing ${nFormatter(Math.floor(getNeededRessource-player.ress[ore]))})`);
-    if (player.ress[ore] >= getNeededRessource) txt.push(`${Emotes[ore]} ${ore} : ${nFormatter(getNeededRessource)} (${Emotes.checked})`);
-
-    embed.addField("**Cost**", txt);
-    embed.addField("**Reward**", `${Emotes[gem]} ${gem} x${quantity}`)
-
-    const msg = await message.channel.send(embed);
-
-    await msg.react(react[0]);
-    await msg.react(react[1]);
-
-    const filter = (reaction, user) => react.includes(reaction.emoji.id) && user.id === message.author.id;
-
-    msg.awaitReactions(filter, { max: 1, time: 30000, errors: ['time'] })
-    .then(collected => {
-        let reaction = collected.first();
-
-        switch(reaction.emoji.id) {
-            case react[0]:
-                msg.delete();
-                let need = [];
-                let resssql = [];
-
-                if (player.data[ore] < getNeededRessource) need.push(`sorry bro`);
-                resssql.push(`${ore} = ${ore} - ${getNeededRessource}`);
-
-                if (need.length >= 1) return message.channel.send(`${lang.prospect.notEnoughRess}`);
-
-                con.query(`UPDATE ress SET ${resssql.join(',')} WHERE userid = ${message.author.id}`);
-                con.query(`UPDATE prospect SET ${gem} = ${player.prospect[gem] + Number(quantity)} WHERE userid = ${message.author.id}`);
-
-                return message.channel.send(`${lang.prospect.success} : **${gem}** x${quantity} !`);
-
-            case react[1]:
-                msg.delete();
-                return message.channel.send(`${lang.prospect.canceled}`);
-        }
-    }).catch(() => {
-        msg.reactions.removeAll();
-    });
-}
+const manageProspecting = require('../../functions/manageProspecting');
+const Discord           = require('discord.js'),
+    Default             = require('../../utils/default.json');
 
 exports.run = async (client, message, args, getPlayer, getUser) => {
-    if (message.author.id !== "436310611748454401") return message.channel.send("Commande en maintenance.");
     const con = client.connection;
     const player = await getPlayer(con, message.author.id);
     if (!player) return message.channel.send(Default.notRegistered);
