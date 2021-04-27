730b9bb97e10ca3bc6c770fa8dea174cd41f6fbc
ReallySouna <zirlax@outlook.com>
2021-04-28 01:22:21 +0200

    wrists spelling fix (enchant.js)

--- commands/RPG/enchant.js
+++ commands/RPG/enchant.js
@@ -13,7 +13,7 @@ exports.run = async (client, message, args, getPlayer, getUser) => {
         .setTitle("ENCHANT")
         .setThumbnail("https://media.discordapp.net/attachments/691992473999769623/796006868212383755/EnchantedDiamondSwordNew.gif")
         .addField("Description", `${lang.enchant.description}`)
-        .addField("Documentation", `${lang.enchant.doc} [ pickaxe ]\n[ sword / shield ]\n[ head / shoulders / chest / wrist\nhands / waist / legs / feet ]`)
+        .addField("Documentation", `${lang.enchant.doc} [ pickaxe ]\n[ sword / shield ]\n[ head / shoulders / chest / wrists\nhands / waist / legs / feet ]`)
         .setTimestamp()
         .setFooter(`${client.user.username}`, client.user.avatarURL());
 
@@ -30,7 +30,7 @@ exports.run = async (client, message, args, getPlayer, getUser) => {
             return manageEnchant(client, con, player, message, "armors", "shoulders", "ench_shoulders")
         case "chest":
             return manageEnchant(client, con, player, message, "armors", "chest", "ench_chest")
-        case "wrist":
+        case "wrists":
             return manageEnchant(client, con, player, message, "armors", "wrists", "ench_wrists")
         case "hands":
             return manageEnchant(client, con, player, message, "armors", "hands", "ench_hands")
