2216e8ca1bb0480d80ddcb75f272320239b11519
ReallySouna <zirlax@outlook.com>
2021-04-29 17:37:59 +0200

    dungeon_stone renamed (profile.js)

--- commands/RPG/profile.js
+++ commands/RPG/profile.js
@@ -27,12 +27,12 @@ exports.run = async (client, message, args, getPlayer, getUser) => {
         }); //end query data
 
         await con.query(`INSERT INTO ress (
-        uuid, userid, energy, zone, dungeon_stone, stone, coal, copper, iron, gold, malachite,
+        uuid, userid, energy, zone, torch, stone, coal, copper, iron, gold, malachite,
         chest_d, chest_c, chest_b, chest_a, chest_s,
         rune_pickaxe, rune_sword, rune_shield, rune_head, rune_shoulders, rune_chest, rune_wrists,
         rune_hands, rune_waist, rune_legs, rune_feet
         ) VALUES (
-        '${Default.player.uuid}', '${message.author.id}', '${Default.player.energy}', '${Default.player.zone}', '${Default.player.dungeon_stone}',
+        '${Default.player.uuid}', '${message.author.id}', '${Default.player.energy}', '${Default.player.zone}', '${Default.player.torch}',
         '${Default.mine.stone}', '${Default.mine.coal}', '${Default.mine.copper}', '${Default.mine.iron}', '${Default.mine.gold}', '${Default.mine.malachite}',
         '${Default.player.chest_d}', '${Default.player.chest_c}', '${Default.player.chest_b}', '${Default.player.chest_a}', '${Default.player.chest_s}',
         '${Default.player.rune_pickaxe}', '${Default.player.rune_sword}', '${Default.player.rune_shield}', '${Default.player.rune_head}', '${Default.player.rune_shoulders}', '${Default.player.rune_chest}', '${Default.player.rune_wrists}',
