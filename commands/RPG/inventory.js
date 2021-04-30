36db2ef0857086620c8a883172e63e0abf0f1261
ReallySouna <zirlax@outlook.com>
2021-04-30 11:24:51 +0200

    new page/PV => HP/Guerrier => Weapons (inventory.js)

--- commands/RPG/inventory.js
+++ commands/RPG/inventory.js
@@ -18,17 +18,17 @@ exports.run = async (client, message, args, getPlayer, getUser) => {
     const embed1 = new Discord.MessageEmbed()
         .setAuthor(`${lang.inventory.inventoryOf} ${message.author.tag}`, message.author.displayAvatarURL())
         .setColor(message.member.displayColor)
-        .setFooter(`Page 1/4 | ${lang.globalHelpFooter}`)
+        .setFooter(`Page 1/5 | ${lang.globalHelpFooter}`)
         .addField(`⭐ Mana`, `${player.data.MANA}/50`, true)
-        .addField(`❤️ PV`, `${player.data.PV}/50`, true)
+        .addField(`❤️ HP`, `${player.data.HP}/50`, true)
         .addField(`⚡ ${lang.inventory.energy} [+1/${moment.duration(cooldown).format("s")}s]`, `${player.ress.energy || 0}/${maxEnergy}`, true)
-        .addField(`📊 ${lang.inventory.stats}:`, `${Emotes.chests.Guerrier.rune_sword} ATK: ${player.data.ATK}\n${Emotes.chests.Guerrier.rune_shield} DEF: ${player.data.DEF}\n${Emotes.chests.Tools.rune_pickaxe} Power : ${player.data.power}`, true)
+        .addField(`📊 ${lang.inventory.stats}:`, `${Emotes.ATK} ATK: ${player.data.ATK}\n${Emotes.DEF} DEF: ${player.data.DEF}\n${Emotes.chests.Tools.rune_pickaxe} Power : ${player.data.power}`, true)
         .addField(`Autres:`, `${Emotes.cash} Balance: ${nFormatter(player.data.money)}\n${Emotes.rep} Reputations : ${player.data.rep}`, true)
 
     const embed2 = new Discord.MessageEmbed()
         .setAuthor(`${lang.inventory.inventory} ${message.author.tag}`, message.author.displayAvatarURL())
         .setColor(message.member.displayColor)
-        .setFooter(`Page 2/4 | ${lang.globalHelpFooter}`)
+        .setFooter(`Page 2/5 | ${lang.globalHelpFooter}`)
 
     var txt = [],
         txt2 = [],
@@ -43,8 +43,8 @@ exports.run = async (client, message, args, getPlayer, getUser) => {
     for (const ressource in Default.pickaxe.R2) {
         txt2.push(`${Emotes[ressource]} ${lang.inventory[ressource]}: ${nFormatter(player.ress[ressource])}`);
     }
-    for (const runes in Default.runes.Guerrier) {
-        txt3.push(`${Emotes.chests.Guerrier[runes]} ${nFormatter(player.ress[runes])}`)
+    for (const runes in Default.runes.Weapons) {
+        txt3.push(`${Emotes.chests.Weapons[runes]} ${nFormatter(player.ress[runes])}`)
     }
     for (const runes in Default.runes.Tools) {
         txt4.push(`${Emotes.chests.Tools[runes]} ${nFormatter(player.ress[runes])}`)
@@ -58,10 +58,16 @@ exports.run = async (client, message, args, getPlayer, getUser) => {
 
     embed2.addField(`${Emotes.ressource} ${lang.inventory.ressources} (1)`, txt, true)
     embed2.addField(`${Emotes.ressource} ${lang.inventory.ressources} (2)`, txt2, true)
-    embed2.addField(`${Emotes.rune} ${lang.inventory.runes}`, `${txt3.join(" ")}\n${txt4.join(" ")}\n${txt5.join(" ")}\n${txt6.join(" ")}`)
-    embed2.addField(`${Emotes.bag} ${lang.inventory.yourObjects}`, `${Emotes.dungeon_stone} ${player.ress.dungeon_stone}`)
-    embed2.addField(`${Emotes.open_chest} ${lang.inventory.chests}`, `${Emotes.chest_d} ${player.ress.chest_d} ${Emotes.chest_c} ${player.ress.chest_c} ${Emotes.chest_b} ${player.ress.chest_b} ${Emotes.chest_a} ${player.ress.chest_a} ${Emotes.chest_s} ${player.ress.chest_s}`)
     embed2.addField(`${Emotes.gem} ${lang.inventory.gems}`, `${Emotes.sapphire} ${player.prospect.sapphire} ${Emotes.amber} ${player.prospect.amber} ${Emotes.citrine} ${player.prospect.citrine} ${Emotes.ruby} ${player.prospect.ruby} ${Emotes.jade} ${player.prospect.jade} ${Emotes.amethyst} ${player.prospect.amethyst}`)
+
+    const embed3 = new Discord.MessageEmbed()
+        .setAuthor(`${lang.inventory.inventory} ${message.author.tag}`, message.author.displayAvatarURL())
+        .setColor(message.member.displayColor)
+        .setFooter(`Page 3/5 | ${lang.globalHelpFooter}`)
+        .addField(`${Emotes.rune} ${lang.inventory.runes}`, `${txt3.join(" ")}\n${txt4.join(" ")}\n${txt5.join(" ")}\n${txt6.join(" ")}`)
+        .addField(`${Emotes.bag} ${lang.inventory.yourObjects}`, `${Emotes.torch} ${player.ress.torch}`)
+        .addField(`${Emotes.open_chest} ${lang.inventory.chests}`, `${Emotes.chest_d} ${player.ress.chest_d} ${Emotes.chest_c} ${player.ress.chest_c} ${Emotes.chest_b} ${player.ress.chest_b} ${Emotes.chest_a} ${player.ress.chest_a} ${Emotes.chest_s} ${player.ress.chest_s}`)
+ 
 
     let pickaxe = Items.tools.pickaxe[player.items.pickaxe];
 
@@ -80,12 +86,12 @@ exports.run = async (client, message, args, getPlayer, getUser) => {
     let dungeon_amulet = Items.objects.dungeon_amulet[player.items.dungeon_amulet]
     let ring = Items.objects.ring[player.items.ring]
 
-    const embed3 = new Discord.MessageEmbed()
+    const embed4 = new Discord.MessageEmbed()
         .setAuthor(`${lang.inventory.inventoryOf} ${message.author.tag}`, message.author.displayAvatarURL())
         .setColor(message.member.displayColor)
-        .setFooter(`Page 3/4 | ${lang.globalHelpFooter}`)
-        .addField(`${Emotes.chests.Guerrier.rune_sword} ${lang.inventory.sword}:`, `${sword.name}\n${lang.inventory.level}: ${player.items.sword}\n[${lang.inventory.enchant}: ${player.enchant.ench_sword}]`, true)
-        .addField(`${Emotes.chests.Guerrier.rune_shield} ${lang.inventory.shield}:`, `${shield.name}\n${lang.inventory.level}: ${player.items.shield}\n[${lang.inventory.enchant}: ${player.enchant.ench_shield}]`, true)      
+        .setFooter(`Page 4/5 | ${lang.globalHelpFooter}`)
+        .addField(`${Emotes.chests.Weapons.rune_sword} ${lang.inventory.sword}:`, `${sword.name}\n${lang.inventory.level}: ${player.items.sword}\n[${lang.inventory.enchant}: ${player.enchant.ench_sword}]`, true)
+        .addField(`${Emotes.chests.Weapons.rune_shield} ${lang.inventory.shield}:`, `${shield.name}\n${lang.inventory.level}: ${player.items.shield}\n[${lang.inventory.enchant}: ${player.enchant.ench_shield}]`, true)      
     
         .addField(`${Emotes.chests.Tools.rune_pickaxe} ${lang.inventory.pickaxe}:`, `${pickaxe.name}\n${lang.inventory.level}: ${player.items.pickaxe}\n[${lang.inventory.enchant}: ${player.enchant.ench_pickaxe}]`, true)
         
@@ -98,19 +104,20 @@ exports.run = async (client, message, args, getPlayer, getUser) => {
         .addField(`${Emotes.chests.Gear.P2.rune_legs} ${lang.inventory.legs}:`, `${legs.name}\n${lang.inventory.level}: ${player.items.legs}\n[${lang.inventory.enchant}: ${player.enchant.ench_legs}]`, true)
         .addField(`${Emotes.chests.Gear.P2.rune_feet} ${lang.inventory.feet}:`, `${feet.name}\n${lang.inventory.level}: ${player.items.feet}\n[${lang.inventory.enchant}: ${player.enchant.ench_feet}]`, true)
 
-    const embed4 = new Discord.MessageEmbed()
-    .setAuthor(`${lang.inventory.inventoryOf} ${message.author.tag}`, message.author.displayAvatarURL())
-    .setColor(message.member.displayColor)
-    .setFooter(`Page 4/4 | ${lang.globalHelpFooter}`)
-    .setDescription(`${Emotes.bag} ${lang.inventory.objects}`)
-    .addField(`${Emotes.dungeon_amulet} ${lang.inventory.dungeon_amulet}:`, `${dungeon_amulet.name}\n${lang.inventory.level}: ${player.items.dungeon_amulet}`, true)
-    .addField(`${Emotes.ring} ${lang.inventory.ring}:`, `${ring.name}\n${lang.inventory.level}: ${player.items.ring}`, true)
+    const embed5 = new Discord.MessageEmbed()
+        .setAuthor(`${lang.inventory.inventoryOf} ${message.author.tag}`, message.author.displayAvatarURL())
+        .setColor(message.member.displayColor)
+        .setFooter(`Page 5/5 | ${lang.globalHelpFooter}`)
+        .setDescription(`${Emotes.bag} ${lang.inventory.objects}`)
+        .addField(`${Emotes.dungeon_amulet} ${lang.inventory.dungeon_amulet}:`, `${dungeon_amulet.name}\n${lang.inventory.level}: ${player.items.dungeon_amulet}`, true)
+        .addField(`${Emotes.ring} ${lang.inventory.ring}:`, `${ring.name}\n${lang.inventory.level}: ${player.items.ring}`, true)
 
     const embeds = [
         embed1,
         embed2,
         embed3,
-        embed4
+        embed4,
+        embed5
     ];
     for (let i = 0; i < 0; ++i)
     embeds.push(new Discord.MessageEmbed().setFooter(`Page ${i + 1}`));
@@ -135,6 +142,8 @@ exports.run = async (client, message, args, getPlayer, getUser) => {
         case "4":
             wow.setPage(4);
             break;
+        case "5":
+            wow.setPage(5);
         default:
             wow.setPage(1);
             break;
@@ -143,9 +152,11 @@ exports.run = async (client, message, args, getPlayer, getUser) => {
 }
 
 module.exports.help = {
-  name: 'inventory',
-  description_fr: 'Affiche votre inventaire',
-  description_en: 'Display your inventory',
-  category: 'RPG',
-  aliases: ['inv', 'i']
+    name: 'inventory',
+    description_fr: 'Affiche votre inventaire',
+    description_en: 'Display your inventory',
+    usage_fr: '(page)',
+    usage_en: '(page)',
+    category: 'RPG',
+    aliases: ['inv', 'i']
 }
