a915b81e83596d13bc6e1c16c957da1bb8a36dee
ReallySouna <zirlax@outlook.com>
2021-04-30 11:25:22 +0200

    gems stats displayed (prospect.js)

--- commands/RPG/prospect.js
+++ commands/RPG/prospect.js
@@ -1,5 +1,6 @@
 const manageProspecting = require('../../functions/manageProspecting');
 const Discord           = require('discord.js'),
+    Emotes              = require('../../utils/emotes.json'),
     Default             = require('../../utils/default.json');
 
 exports.run = async (client, message, args, getPlayer, getUser) => {
@@ -14,7 +15,7 @@ exports.run = async (client, message, args, getPlayer, getUser) => {
         .attachFiles(["./utils/images/gem_thumbnail_prospect.png"])
         .setThumbnail("attachment://gem_thumbnail_prospect.png")
         .addField("Description", `${lang.prospect.description}`)
-        .addField("Documentation", `${lang.prospect.doc} [ stone/coal/copper/iron/gold/malachite ]`)
+        .addField("Documentation", `${lang.prospect.doc}10k ${Emotes.ressource} = 1 ${Emotes.gem}\n\n${Emotes.stone} => ${Emotes.sapphire} +1 Power\n${Emotes.coal} => ${Emotes.amber} -1 sec on energy cooldown\n${Emotes.copper} => ${Emotes.citrine} +1 Mana Max\n${Emotes.iron} => ${Emotes.ruby} +1 HP Max\n${Emotes.gold} => ${Emotes.jade} +1 ATK\n${Emotes.malachite} => ${Emotes.amethyst} +1 DEF`)
         .setTimestamp()
         .setFooter(`${client.user.username}`, client.user.avatarURL());
     
@@ -24,17 +25,17 @@ exports.run = async (client, message, args, getPlayer, getUser) => {
 
     switch (args[0]) {
         case "stone":
-            return manageProspecting(client, con, player, message, "stone", args[1], "sapphire");
+            return manageProspecting(client, con, player, message, "stone", args[1], "sapphire", "+Power");
         case "coal":
-            return manageProspecting(client, con, player, message, "coal", args[1], "amber");
+            return manageProspecting(client, con, player, message, "coal", args[1], "amber", "-energy cooldown");
         case "copper":
-            return manageProspecting(client, con, player, message, "copper", args[1], "citrine");
+            return manageProspecting(client, con, player, message, "copper", args[1], "citrine", "+Mana Max");
         case "iron":
-            return manageProspecting(client, con, player, message, "iron", args[1], "ruby");
+            return manageProspecting(client, con, player, message, "iron", args[1], "ruby", "+HP Max");
         case "gold":
-            return manageProspecting(client, con, player, message, "gold", args[1], "jade");
+            return manageProspecting(client, con, player, message, "gold", args[1], "jade", "+ATK");
         case "malachite":
-            return manageProspecting(client, con, player, message, "malachite", args[1], "amethyst");
+            return manageProspecting(client, con, player, message, "malachite", args[1], "amethyst", "+DEF");
         default:
             return message.channel.send(prospectEmbed);
     }
@@ -42,8 +43,8 @@ exports.run = async (client, message, args, getPlayer, getUser) => {
 
 exports.help = {
     name: "prospect",
-    description_fr: "Cherche des gemmes précieuses dans 5 minerais d'un métal de faible valeur. Cela détruira les minerais.",
-    description_en: "Look for precious gems in 5 ores of a low value metal. This will destroy the ores.",
+    description_fr: "Cherche des gemmes précieuses dans 10 000 minerais d'un métal de faible valeur. Cela détruira les minerais.",
+    description_en: "Look for precious gems in 10 000 ores of a low value metal. This will destroy the ores.",
     usage_fr: "<minerai> <quantité>",
     usage_en: "<ore> <quantity>",
     category: "RPG",
