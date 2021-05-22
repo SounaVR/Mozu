79a919d2ea19cfe9afe5e9b4dbc9a44599d4fa40
ReallySouna <zirlax@outlook.com>
2021-05-22 18:05:48 +0200

    reworked (manageGive.js)

--- functions/manageGive.js
+++ functions/manageGive.js
@@ -2,44 +2,42 @@ const Emotes  = require('../utils/emotes.json');
 
 module.exports = function manageGive(client, con, args, player, member, message, objectName, objectAliases) {
     var con = client.connection
-    const lang = require(`../utils/text/${player.data.lang}.json`)
+    const lang = require(`../utils/text/${player.data.lang}.json`);
+    const react = ["780222056007991347", "780222833808506920"];
     const user = message.mentions.users.first();
     const userid = message.author.id;
 
     if (args[1] && objectAliases.includes(args[1].toLowerCase()) || objectName.includes(args[1].toLowerCase())) {
-        args[2] = Math.floor(args[2])
-
         if (args[2] > 0) {
-            message.channel.send(`${lang.give.wantGive} **${args[2]} ${lang.inventory[objectName]}**${Emotes[objectName]} ${lang.give.wantGiveTo} **${user}** ?`).then(async e => {
-                await e.react("✅");
-                await e.react("❌");
+            message.reply(`${lang.give.wantGive.replace("%s", `**${args[2]} ${lang.inventory[objectName]}**${Emotes[objectName]}`).replace("%u", user)}`).then(async e => {
+                await e.react(react[0]);
+                await e.react(react[1]);
 
-                const filter = (reaction, user) => {
-                    return ['✅', '❌'].includes(reaction.emoji.name) && user.id === message.author.id;
-                };
+                const filter = (reaction, user) => react.includes(reaction.emoji.id) && user.id === message.author.id;
 
                 e.awaitReactions(filter, { max: 1, time: 45000, errors: ['time'] })
                 .then(async collected => {
                     const reaction = collected.first();
 
-                    if (reaction.emoji.name === '✅') {
-                        if (player.ress[objectName] < args[2]) {
-                            e.reactions.removeAll();
-                            return e.edit(`${lang.give.notEnoughRess}`)
-                        }
-                        con.query(`UPDATE ress SET ${objectName} = ${player.ress[objectName] - args[2]} WHERE userid = ${userid}`)
-                        con.query(`UPDATE ress SET ${objectName} = ${member.ress[objectName] + Number(args[2])} WHERE userid = ${user.id}`)
-                        e.edit(`${lang.give.giveSuccess} **${args[2]} ${lang.inventory[objectName]}**${Emotes[objectName]} ${lang.give.giveSuccessTo} **${user}** !`)
-                    } else if (reaction.emoji.name === '❌') {
-                        e.edit(`${lang.give.canceled}`);
+                    switch (reaction.emoji.id) {
+                        case react[0]:
+                            if (player.ress[objectName] < args[2]) {
+                                e.reactions.removeAll();
+                                return e.edit(`${Emotes.cancel} ${lang.give.notEnoughRess}`);
+                            }
+                            con.query(`UPDATE ress SET ${objectName} = ${player.ress[objectName] - args[2]} WHERE userid = ${userid}`);
+                            con.query(`UPDATE ress SET ${objectName} = ${member.ress[objectName] + Number(args[2])} WHERE userid = ${user.id}`);
+                            e.edit(`${Emotes.checked} ${lang.give.giveSuccess.replace("%m", `**${args[2]} ${lang.inventory[objectName]}**${Emotes[objectName]}`).replace("%u", `**${user}**`)}`);
+                            break;
+                        case react[1]:
+                            e.edit(`${Emotes.cancel} ${lang.give.canceled}`);
+                            break;
                     }
                     e.reactions.removeAll();
                 }).catch(collected => {
                     e.reactions.removeAll();
                 })
             });
-        } else {
-        return message.channel.send(`${lang.give.needSpecifyAmount}`)
         }
     }
 }
