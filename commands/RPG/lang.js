13b4b4b5fbea5e6ffb7f2cd6eef711cce1eaa399
ReallySouna <zirlax@outlook.com>
2021-05-22 18:03:28 +0200

    lang replace (lang.js)

--- commands/RPG/lang.js
+++ commands/RPG/lang.js
@@ -4,21 +4,21 @@ module.exports.run = async (client, message, args, getPlayer, getUser) => {
     var con = client.connection
     var player = await getPlayer(con, message.author.id);
     if (!player) return message.channel.send(Default.notRegistered);
-    const userid = message.author.id;
 
     if (args[0] && ["en", "EN"].includes(args[0].toLowerCase())) {
-        con.query('UPDATE data SET lang = ? WHERE userid = ?', ["en", userid])
+        con.query(`UPDATE data SET lang = "en" WHERE userid = ${message.author.id}`)
         var player = await getPlayer(con, message.author.id);
         const lang = require(`../../utils/text/${player.data.lang}.json`);
-        return message.channel.send(lang.confirmLanguage);
+        return message.channel.send(lang.confirmLanguage.replace("%s", "`EN`"));
     } else if (args[0] && ["fr", "FR", "france", "français"].includes(args[0].toLowerCase())) {
-        con.query('UPDATE data SET lang = ? WHERE userid = ?', ["fr", userid])
+        con.query(`UPDATE data SET lang = "fr" WHERE userid = ${message.author.id}`)
         var player = await getPlayer(con, message.author.id);
         const lang = require(`../../utils/text/${player.data.lang}.json`);
-        return message.channel.send(lang.confirmLanguage);
+        return message.channel.send(lang.confirmLanguage.replace("%s", "`FR`"));
     } else {
-        if (player.data.lang === "fr") return message.channel.send("Langues disponibles : `EN/FR`");
-        if (player.data.lang === "en") return message.channel.send("Available languages : `EN/FR`");
+        var player = await getPlayer(con, message.author.id);
+        const lang = require(`../../utils/text/${player.data.lang}.json`);
+        return message.channel.send(lang.availableLanguages);
     }
 // 🇫🇷 🇺🇸
 };
