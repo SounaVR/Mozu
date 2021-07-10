8103d36cbe9539f2e11a3d9a2b6a58a117d5fc80
Kinchi <sounafr@protonmail.com>
2021-07-10 17:57:55 +0200

    energyCooldown added (insert_data.js)

--- functions/insert/insert_data.js
+++ functions/insert/insert_data.js
@@ -3,12 +3,12 @@ const Default = require('../../utils/default.json');
 module.exports = async function insert_data(client, con, player, message, databaselogs, userid) {
     if (!player) {
         await con.query(`INSERT INTO data (
-            uuid, username, userid, lang, ban, money, lastActivity,
+            uuid, username, userid, lang, ban, money, energyCooldown, astActivity,
             HP, MANA, ATK, DEF, power,
             HR, lastHR, daily, lastDaily, rep, lastRep
             ) VALUES (
             '${Default.player.uuid}', '${message.author.username}#${message.author.discriminator}', '${message.author.id}', '${Default.player.lang}', '${Default.player.ban}',
-            '${Default.player.money}', '${Default.player.lastActivity}',
+            '${Default.player.money}', '${Default.player.energyCooldown}','${Default.player.lastActivity}',
             '${Default.player.HP}', '${Default.player.MANA}', '${Default.player.ATK}', '${Default.player.DEF}', '${Default.player.power}',
             '${Default.player.HR}', '${Default.player.lastHR}', '${Default.player.daily}', '${Default.player.lastDaily}', '${Default.player.rep}', '${Default.player.lastRep}'
         )`, async function(err) {
