11d329d5789241b9d22f5e5dccd0555ac00abc74
Kinchi <sounafr@protonmail.com>
2021-07-16 04:41:54 +0200

    boost finally working

--- commands/RPG/boost.js
+++ commands/RPG/boost.js
@@ -1,30 +1,20 @@
-require("moment-duration-format");
-// const { timeFormating } = require('timeformatter');
+const { getPremiumDuration } = require("../../utils/u");
 const Discord     = require('discord.js'),
-    Emotes        = require('../../utils/emotes.json'),
-    moment        = require("moment");
-moment.locale("fr");
+    Emotes        = require('../../utils/emotes.json');
 
 exports.run = async (client, message, args, getPlayer) => {
-    // var con = client.connection
-    // var player = await getPlayer(con, message.author.id);
-    // if (!player) return message.channel.send("You are not registered, please do the `m!village` command to remedy this.")
-    // const lang = require(`../../utils/text/${player.data.lang}.json`);
     const userid = message.author.id;
 
     const booster = message.guild.members.cache.get(userid);
-    var isBooster = message.member.premiumSinceTimestamp ? moment(Date.now() - message.member.premiumSinceTimestamp).format("Y[y] M[m] D[j] HH[h] m[m] s[s]") : "None"
+
+    const b = getPremiumDuration(booster);
+    var text = booster.premiumSinceTimestamp ? `${b.years}y ${b.months}m ${b.days}j ${b.hours}h ${b.minutes}m ${b.seconds}s` : "None"
 
     const embed = new Discord.MessageEmbed()
     .setTitle("Boost Informations")
-    .setDescription(`Semaines de boost : **X**
-        Niveau de boost : **X** ${Emotes.boost1}
-        Récompense actuelle : **X**
-        Boost depuis : **${isBooster}**`)
-    //moment(booster.premiumSinceTimestamp).format("M[m] D[j] HH[h] m[m] s[s]")
+    .setColor("#f47fff")
+    .setDescription(`Boost depuis : **${text}**`)
     return message.channel.send(embed);
-    //.premiumSince
-    //.premiumSinceTimestamp
 };
 
 exports.help = {
