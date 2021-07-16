784ce73775b143985162305a6ee0c21f843411ef
Kinchi <sounafr@protonmail.com>
2021-07-16 04:42:40 +0200

    drop added (guide.js)

--- commands/Staff/guide.js
+++ commands/Staff/guide.js
@@ -4,13 +4,14 @@ exports.run = async (client, message, args, getPlayer, getUser) => {
     message.author.send(`Guide détaillé des commandes Staff :
     **m!ban [@user]** : Quand vous mentionnez quelqu'un, il se verra banni du bot. En d'autres mots, il pourra plus exécuter de commandes du tout avant l'unban.
     **m!unban [@user]** : Fait l'exact inverse du ban.
-    **m!cheat [@user] [set/add] [data/ress/items/enchant/prospect/stats] [colonne (<#795598892053561364>)] [valeur]** : Modifie une donnée dans la database.
+    **m!cheat [@user] [set/add] [data/enchant/items/prospect/ress/slots/stats] [colonne] [valeur]** : Modifie une donnée dans la database.
     ⚠ Ne modifiez surtout pas "uuid" et "userid", merci. ⚠
     **m!eval [code]** : Permet "d'évaluer" du code, comme changer le pseudo de quelqu'un ou autre, à utiliser uniquement en cas de nécessité.*
     **m!exec [commande]** : Owner only, c'est des commandes dans le vps (vous pouvez carrément le reboot si vous y avez accès).
     **m!reboot** : Redémarre le bot.
     **m!reload [commande]** : Recharge une commande.
     **m!sql [requête]** : Owner only, permet de faire des requêtes sql à la manière du m!cheat mais en plus compliqué.
+    **m!drop [@user]** : Owner only, supprimer __l'entièreté__ des données de la database de l'utilisateur mentionné.
     
     Pour voir les aliases, faites m!help [commande].`);
     return message.react("✅");
