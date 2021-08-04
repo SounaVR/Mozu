2b7d835dd49ddd0d69ad4dd1d2af21d46e5ca57c
Kinchi <sounafr@protonmail.com>
2021-08-04 17:47:23 +0200

    gif pp works (userinfo.js)

--- commands/Infos/userinfo.js
+++ commands/Infos/userinfo.js
@@ -7,9 +7,9 @@ exports.run = async (client, message, args, getPlayer, getUser) => {
 
     if (member) {
     const embed = new Discord.MessageEmbed()
-        .setAuthor(`${member.user.tag} (${member.id})`, member.user.displayAvatarURL())
+        .setAuthor(`${member.user.tag} (${member.id})`, member.user.avatarURL({ dynamic: true, size: 512 }))
         .setColor(message.member.displayColor)
-        .setThumbnail(member.user.displayAvatarURL())
+        .setThumbnail(member.user.avatarURL({ dynamic: true, size: 512 }))
         .addField('Created On', `${moment.utc(member.user.createdAt).format("DD/MM/YYYY")} (${checkDays(member.user.createdAt)})`, true)
         .addField('Joined On', `${moment.utc(member.user.joinedAt || member.joinedAt).format("DD/MM/YYYY")} (${checkDays(member.user.joinedAt || member.joinedAt)})`, true)
         .addField("Nickname", `${member.nickname != null ? `${member.nickname}` : 'None'}`)
