450dd63ea66953403ebcc0a2a2eecd2a10df4b01
Kinchi <sounafr@protonmail.com>
2021-08-15 17:17:27 +0200

    better botinfo

--- commands/Infos/botinfo.js
+++ commands/Infos/botinfo.js
@@ -8,7 +8,7 @@ exports.run = async (client, message, args, getPlayer, getUser) => {
 	con.query(`SELECT COUNT(*) AS usersCount FROM data`, function (err, rows, fields) {
 		if (err) throw err;
 
-		const duration = moment.duration(client.uptime).format("D [days], HH [hrs], m [mins], s [secs]");
+		const duration = moment.duration(client.uptime).format("DD [days], HH [hrs], mm [mins], ss [secs]");
 		const test = moment(client.user.createdAt).format("DD/MM/YYYY");
 		const developer = client.users.cache.find(user => user.id === client.config.owners[0]);
 		let bicon = client.user.avatarURL();
@@ -16,21 +16,21 @@ exports.run = async (client, message, args, getPlayer, getUser) => {
 			.setAuthor(`${client.user.username}`, bicon)
 			.setColor(message.member.displayColor)
 			.setThumbnail(bicon)
-			.addField("Developer", developer.tag, true)
-			.addField("Uptime", duration, true)
-			.addField("Programming language", "Node.js", true)
-
-			.addField("API", "discord.js", true)
-			.addField("Memory used", Math.floor(process.memoryUsage().heapUsed / 1024 / 1024) + " Mb", true)
-			.addField("Players", `${rows[0].usersCount} players`, true)
-
-			.addField("Servers", `${client.guilds.cache.size} servers`, true)
-			.addField("Users", `${client.users.cache.size} users`, true)
-			.addField("My link", "[Don't click](https://media.discordapp.net/attachments/706350683766390854/845538726162989076/3237789807_1_3_brmovBmI.png)", true)
+			.addField("⚒️ Developer", developer.tag)
 			
-			.addField("Creation Date", `${test}\n(${checkDays(client.user.createdAt)})`, true)
+			.addField("🕒 Uptime", duration, true)
+			.addField("🖥️ Programming language", "Node.js", true)
+			.addField("🌐 API", "discord.js", true)
 
-			.setFooter(`${client.user.username}`, client.user.avatarURL())
+			.addField("<:RAM:875793593778970624> Memory used", Math.floor(process.memoryUsage().heapUsed / 1024 / 1024) + " Mb", true)
+			.addField("🕹️ Players", `${rows[0].usersCount}`, true)
+			.addField("<:Discord:875795160498315294> Servers", `${client.guilds.cache.size}`, true)
+
+			.addField("👥 Users", `${client.users.cache.size}`, true)
+			.addField("📎 My link", "[Don't click](https://media.discordapp.net/attachments/706350683766390854/845538726162989076/3237789807_1_3_brmovBmI.png)", true)
+			.addField("📆 Creation Date", `${test}\n(${checkDays(client.user.createdAt)})`, true)
+
+			.setFooter(`${client.user.username}`, client.user.avatarURL({ dynamic: true }))
 			.setTimestamp();
 		message.channel.send({ embeds: [botembed] });
 	});
