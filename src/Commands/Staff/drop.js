a5cadb1f22bdbc0d1ee4426a18fd28a9bd0f9961
Souna <59102661+SounaVR@users.noreply.github.com>
2023-01-20 09:30:31 +0100

    delete user command from db

---
+++ src/Commands/Staff/drop.js
@@ -0,0 +1,43 @@
+const { SlashCommandBuilder } = require('discord.js');
+const Default = require('../../../utils/default.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('drop')
+        .setDescription('Delete a player from the database.')
+        .setNameLocalizations({
+            fr: "drop",
+            "en-US": "drop"
+        })
+        .setDescriptionLocalizations({
+            fr: "Supprime un joueur de la base de données.",
+            "en-US": "Delete a player from the database."
+        })
+        .addUserOption(option =>
+            option
+                .setName("user")
+                .setDescription("Select an user.")
+                .setDescriptionLocalizations({
+                    fr: "Sélectionnez un utilisateur.",
+                    "en-US": "Select an user."
+                })
+                .setRequired(true)
+        ),
+    async execute(client, interaction) {
+        const { user, options } = interaction;
+        if (user.id !== "436310611748454401") return interaction.reply('❌');
+        const target = options.getUser('user');
+
+        const con = client.connection;
+
+        await con.query(`DELETE FROM data WHERE userid = ${target.id}`);
+        await con.query(`DELETE FROM enchant WHERE userid = ${target.id}`);
+        await con.query(`DELETE FROM items WHERE userid = ${target.id}`);
+        await con.query(`DELETE FROM prospect WHERE userid = ${target.id}`);
+        await con.query(`DELETE FROM ress WHERE userid = ${target.id}`);
+        await con.query(`DELETE FROM slots WHERE userid = ${target.id}`);
+        await con.query(`DELETE FROM stats WHERE userid = ${target.id}`);
+
+        await interaction.reply(`${target}/${target.id} has been DESTROYED :boom:.`);
+    }
+}
