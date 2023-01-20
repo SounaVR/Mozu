c27fbf39b6b9a7092e83e2cb1618ecb0a5e77a40
Souna <59102661+SounaVR@users.noreply.github.com>
2023-01-20 09:28:56 +0100

    Class Client

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 require('dotenv').config();
-const { Client, Collection } = require('discord.js');
-const EventHandler = require('../Handlers/Events');
+const Mozu = require('../Classes/Client');
 
-const client = new Client(({
-    intents: 1799,
-    allowedMentions: { parse: ["users", "roles"], repliedUser: true },
-    restTimeOffset: 250
-}));
-
-client.commands = new Collection();
-EventHandler(client);
+const client = new Mozu();
 
 client.login(process.env.BOT_TOKEN);
