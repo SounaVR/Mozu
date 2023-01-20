a8819cf971bcaf2004d97795978e17c442b687a5
Souna <59102661+SounaVR@users.noreply.github.com>
2023-01-20 09:28:46 +0100

    badges system

--- functions/insert/insert_data.js
+++ functions/insert/insert_data.js
@@ -4,13 +4,13 @@ module.exports = async function insert_data(client, con, player, interaction, da
     if (!player) {
         await con.query(`INSERT INTO data (
             uuid, username, userid,
-            lang, ban,
+            lang, ban, badges,
             money, manaCooldown, hpCooldown, energyCooldown, lastActivity,
             HP, MANA, ATK, DEF, power,
             lastHR, lastDaily, lastRep
             ) VALUES (
             '${Default.player.uuid}', '${interaction.user.tag}', '${userid}',
-            '${Default.player.lang}', '${Default.player.ban}',
+            '${Default.player.lang}', '${Default.player.ban}', '${Default.badges.default}',
             '${Default.player.money}', '${Default.player.manaCooldown}', '${Default.player.hpCooldown}', '${Default.player.energyCooldown}', '${Default.player.lastActivity}',
             '${Default.player.HP}', '${Default.player.MANA}', '${Default.player.ATK}', '${Default.player.DEF}', '${Default.player.power}',
             '${Default.player.lastHR}', '${Default.player.lastDaily}', '${Default.player.lastRep}'
