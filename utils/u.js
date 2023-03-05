0bc00612e3c4bd200ca7df4432d74ef9296f63e4
Souna <59102661+SounaVR@users.noreply.github.com>
2023-03-05 16:28:08 +0100

    more functions documentation + paginator by me

--- utils/u.js
+++ utils/u.js
@@ -1,10 +1,14 @@
 const { format } = require("util");
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType } = require("discord.js");
 
 module.exports = {
     /**
      * @param {client.connection} con 
-     * @param {userid} player 
-     * @returns {Promise<object>}
+     * @param {userid} player
+     * @example
+     * const player = getPlayer(con, interaction.user.id);
+     * console.log(player.data.userid);
+     * @returns {Promise<object>} Returns the whole desired column from anywhere in the code
      */
     getPlayer: async function(con, player) {
         return new Promise(function(resolve, reject) {
@@ -34,9 +38,9 @@ module.exports = {
     },
 
     /**
-     * @param {number} num
+     * @param {number} num The wanted number to be crunched
      * @example nFormatter(40000) > '40k'
-     * @returns {string}
+     * @returns A formatted typology of a number
      */
      nFormatter: function(num) {
         const format = [
@@ -53,9 +57,9 @@ module.exports = {
     },
 
     /**
-     * @param {timestamp} date
+     * @param {timestamp} date The timestamp to convert
      * @example checkDays(1589788800) > '1 day ago'
-     * @returns {string}
+     * @returns The number of day based on a Timestamp
      */
     checkDays: function(date) {
         let now = new Date();
@@ -65,8 +69,8 @@ module.exports = {
     },
 
     /**
-     * @param {member} member
-     * @returns {object} complete date since member boosting
+     * @param {member} member Reminder : "member" is an user from a guild, not the user outside the server
+     * @returns complete date since member boosting
      * @example
      * const booster = interaction.guild.members.cache.get(interaction.user.id);
      * const boostDate = getPremiumDuration(booster);
@@ -93,17 +97,131 @@ module.exports = {
     },
 
     /**
-     * @param {string} lang
+     * @param {string} lang The desired language to be translated
      * @param {string} path to the JSON string
      * @param {string} args to replace in the string
-     * @returns {string} the translated string
+     * @example translate(player.data.lang, 'bal.actualBal', `**${nFormatter(player.data.money)}**💰`)
+     * @returns the translated string
      */
     translate: function(lang, path, ...args) {
         const langFile = require('../utils/Text/' + lang + '.json');
         path = path.split('.').reduce((o,i) => o[i], langFile);
         return format(path, ...args);
     },
+
     sleep: function(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
+    },
+
+    /**
+     * @param {interaction} interaction The discord interaction message
+     * @param {array} pages The array who contains the embeds
+     * @returns The embed which contain the paginator (or without any components if there's only one page)
+     */
+    buttonPages: async function(interaction, pages, time = 60000) {
+        // errors
+        if (!interaction) throw new Error("Please provide an interaction argument.");
+        if (!pages) throw new Error("Please provide a page argument.");
+        if (!Array.isArray(pages)) throw new Error("Pages must be an array");
+
+        if (typeof time !== "number") throw new Error("Time must be a number.");
+        if (parseInt(time) < 30000)
+            throw new Error("Time must be greater than 30 Seconds.");
+
+        // defer reply
+        await interaction.deferReply();
+
+        // no buttons if there is only one page
+        if (pages.length === 1) {
+            const page = await interaction.editReply({
+                embeds: pages,
+                components: [],
+                fetchReply: true
+            });
+
+            return page;
+        }
+
+        // adding buttons
+        const prev = new ButtonBuilder()
+            .setCustomId("prev")
+            .setEmoji("⬅")
+            .setStyle(ButtonStyle.Primary)
+            .setDisabled(true);
+        
+        const home = new ButtonBuilder()
+            .setCustomId("home")
+            .setEmoji("🏠")
+            .setStyle(ButtonStyle.Danger)
+            .setDisabled(true);
+        
+        const next = new ButtonBuilder()
+            .setCustomId("next")
+            .setEmoji("➡")
+            .setStyle(ButtonStyle.Primary);
+
+        const buttonRow = new ActionRowBuilder().addComponents(prev, home, next);
+        let index = 0;
+
+        const currentPage = await interaction.editReply({
+            embeds: [pages[index]],
+            components: [buttonRow],
+            fetchReply: true
+        });
+
+        // creating the collector
+        const collector = await currentPage.createMessageComponentCollector({
+            ComponentType: ComponentType.Button,
+            time
+        });
+
+        collector.on("collect", async (i) => {
+            if (i.user.id !== interaction.user.id)
+                return i.reply({
+                    content: "You can't use these buttons",
+                    ephemeral: true
+                });
+
+            await i.deferUpdate();
+
+            switch (i.customId) {
+                case "prev":
+                    if (index > 0) index--;
+                    break;
+            
+                case "home":
+                    index = 0;
+                    break;
+                
+                case "next":
+                    if (index < pages.length - 1) index++;
+                    break;
+            }
+
+            if (index === 0) prev.setDisabled(true);
+            else prev.setDisabled(false);
+
+            if (index === 0) home.setDisabled(true);
+            else home.setDisabled(false);
+
+            if (index === pages.length - 1) next.setDisabled(true);
+            else next.setDisabled(false);
+
+            await currentPage.edit({
+                embeds: [pages[index]],
+                components: [buttonRow]
+            });
+
+            collector.resetTimer();
+        });
+
+        // ending the collector
+        collector.on("end", async (i) => {
+            await currentPage.edit({
+                embeds: [pages[index]],
+                components: []
+            });
+        });
+        return currentPage;
     }
 }
