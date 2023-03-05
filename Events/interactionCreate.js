ed2d2961373cbf6e28e8cbde74951ea5981ae599
Souna <59102661+SounaVR@users.noreply.github.com>
2023-03-05 16:25:50 +0100

    passive energy regeneration back

--- Events/interactionCreate.js
+++ Events/interactionCreate.js
@@ -4,6 +4,8 @@ module.exports = {
 	name: Events.InteractionCreate,
 	async execute(client, interaction) {
 		if (!interaction.isCommand()) return;
+		const con = client.connection;
+		const player = await client.getPlayer(con, interaction.user.id);
 
 		const command = interaction.client.commands.get(interaction.commandName);
 
@@ -13,7 +15,24 @@ module.exports = {
 		}
 
 		try {
-			await command.execute(client, interaction);
+			if (player) {
+				const Items = require(`../utils/Items/${player.data.lang}.json`);
+				const maxEnergy = Items.objects.ring[player.items.ring].energy;
+				const energyCooldown = player.data.energyCooldown;
+				con.query(`UPDATE stats SET cmd = ${player.stats.cmd + Number(1)} WHERE userid = ${interaction.user.id}`);
+	
+				if ((Date.now() - player.data.lastActivity) - energyCooldown > 0) {
+					const timeObj = Date.now() - player.data.lastActivity;
+					const gagnees = Math.floor(timeObj / energyCooldown);
+			
+					player.ress.energy = (player.ress.energy || 0) + gagnees;
+					if (player.ress.energy > maxEnergy) player.ress.energy = maxEnergy;
+					con.query(`UPDATE ress SET energy = ${player.ress.energy} WHERE userid = ${interaction.user.id}`);
+					con.query(`UPDATE data SET lastActivity = ${Date.now()} WHERE userid = ${interaction.user.id}`);
+				}
+			}
+
+			await command.execute(client, interaction);		
 		} catch (error) {
 			console.error(error);
 		}
