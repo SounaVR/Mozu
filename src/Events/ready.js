0643dc9f467178f2feff2e3795ab849df507dd78
SounaVR <pinpoye21@gmail.com>
2023-06-02 23:40:03 +0200

    daily reset

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -1,9 +1,10 @@
 const { Events, ActivityType } = require('discord.js');
-const dayjs = require('dayjs');
 const duration = require('dayjs/plugin/duration');
+const dayjs = require('dayjs');
+const cron = require('cron');
 const fs = require('fs');
-dayjs.extend(duration);
 
+dayjs.extend(duration);
 dayjs.locale("fr");
 
 module.exports = {
@@ -24,6 +25,22 @@ module.exports = {
 
         client.user.setActivity('death.', { type: ActivityType.Competing });
         client.user.setStatus('dnd');
+
+        // const { exec } = require ('child_process');
+        const channel = client.channels.cache.find(ch => ch.id === '1106251984580911124');
+        const dailyReset = new cron.CronJob('00 00 * * *', async () => {
+            try {
+                // exec(`mysqldump --all-databases --single-transaction --quick --lock-tables=false > ./backups/full-backup-$(date +%F).sql -u ${process.env.BACKUP_USERNAME} -p ${process.env.BACKUP_PASSWORD}`);
+                // exec(`zsh -c 'rm **/*(.om[-1])'`);
+                client.query(`UPDATE data SET LastRep = 0, LastDaily = 0`);
+                // channel.send(`🟢 Daily backup done.`);
+            } catch (error) {
+                channel.send("🔴 An error occurred.");
+                if (error) throw error;
+            }
+        });
+
+        dailyReset.start();
 
         console.log("Bot ready ✅");
     }
